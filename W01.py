deccecd8dda6c6eb262994d2305e74a26f9ee8b9
peetel22 <peetel22@hotmail.com>
2025-06-19 17:50:34 +0700

    first commit

---
+++ W01.py
@@ -0,0 +1,6 @@
+# INPUT  DATA
+name = input("กรุณากรอกชื่อของท่าน :")
+lname = input("กรุณากรอกนามสกุลของท่าน :")
+
+print("ชื่อ :",name,"นามสกุล :",lname)
+print(f"ชื่อ : {name} นามสกุล : {lname}")
